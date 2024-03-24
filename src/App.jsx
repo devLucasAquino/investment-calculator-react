7c81d6349543f690858831764d1a8400556eb11a
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-03-24 17:42:11 -0300

    init input area

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    
+    <div id="user-input">
+
+      <label htmlFor="">initial investment</label>
+      <input type="text" />
+
+      <label htmlFor="">anual investment</label>
+      <input type="text" />
+
+      <label htmlFor="">expected return</label>
+      <input type="text" />
+
+      <label htmlFor="">duration</label>
+      <input type="number" />
+    </div>
+    )
 }
 
 export default App
