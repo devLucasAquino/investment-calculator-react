53cddafb2240e82ea0f1409120d26d806ee9e2c1
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-04-28 13:09:26 -0300

    feat: added Header component at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
+import Header from "./components/Header"
 
 function App() {
   return (
     
-    <div id="user-input">
-
-      <label htmlFor="">initial investment</label>
-      <input type="text" />
-
-      <label htmlFor="">anual investment</label>
-      <input type="text" />
-
-      <label htmlFor="">expected return</label>
-      <input type="text" />
-
-      <label htmlFor="">duration</label>
-      <input type="number" />
-    </div>
+    <Header />
+    
     )
 }
 
