4de0511fc7e18a5460068f6eec172c6b7946f61b
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-05-03 17:00:32 -0300

    fix: correct possible errors

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ function App() {
     duration: 10
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue){
     setUserInput(prevUserInput => {
         return{
@@ -27,7 +29,14 @@ function App() {
     
     <Header />
     <UserInput userInput={userInput} onChangeInput={handleChange}/>
-    <Results input={userInput}/>
+
+    {!inputIsValid && <p className="center"> Please enter a duration greater than zero.</p>}
+    {inputIsValid && <Results input={userInput}/>}
+
+    {/* {inputIsValid ? 
+      <Results input={userInput}/> : 
+      <p className="center"> Please enter a duration greater than zero.</p>} */}
+
 
     </>
     
